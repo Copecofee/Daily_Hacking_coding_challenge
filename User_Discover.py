1d02765950e5e37bca514e1384e5d4e8efb81abb
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-06-19 16:44:59 -0300

    Add files via upload

---
+++ User_Discover.py
@@ -0,0 +1,24 @@
+#Importing Modules
+
+from os import *
+from colorama import *
+init(autoreset=True)
+
+#Clearing the screen
+
+system("clear")
+
+def main():
+ checking = getlogin()
+ users = listdir("/home")
+ print("The user of the system is: "+Fore.BLUE+f"{checking}")
+
+ for x in users:
+   print("Users found: "+Fore.RED+f"{x} ")
+
+ #A funcao len() conta quantos items tem na lista
+
+ print("Total Users: ", len(users))
+
+if __name__ == "__main__":
+ main()
