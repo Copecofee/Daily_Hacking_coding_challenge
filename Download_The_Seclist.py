43ae38f8a0b6f5cbf82ae1a0990ecb8abe263497
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-06-19 16:00:20 -0300

    Add files via upload

---
+++ Download_The_Seclist.py
@@ -0,0 +1,12 @@
+#Made byCopecofee
+
+#WARNING: Be careful if you use this script because when you download the Seclist will be use 834.5 MB from your storaged 
+
+
+from os import system as exe
+
+def main_code():
+ exe(" git clone https://github.com/danielmiessler/SecLists")
+
+if __name__ == "__main__":
+ main_code()
