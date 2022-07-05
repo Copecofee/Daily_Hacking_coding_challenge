bdf97719fc67827d82af6f3a381078f1829b26ef
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-07-05 14:14:11 -0300

    Add files via upload

---
+++ yt_downloader.py
@@ -0,0 +1,56 @@
+#Made by Copecofee Date: 04/07/2022
+#
+# The rights to: https://dev.to/stokry/download-youtube-video-to-mp3-with-python-26p
+# Thanks too for the blog: https://www.instagram.com/p/CetRuufFs-z/
+# I just could develop part of this project thanks to analyze the exploit: https://www.exploit-db.com/exploits/50963 <- EXPLOIT
+
+# DOCUMENATION: https://docs.python.org/3/library/argparse.html
+
+import argparse
+import pytube
+from colorama import *
+from os import system as exe
+import youtube_dl
+
+init(autoreset=True)
+
+exe("clear")
+
+banner = """
+██╗   ██╗ ██████╗ ██╗   ██╗████████╗██╗   ██╗██████╗ ███████╗    ██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗ ███████╗██████╗ 
+╚██╗ ██╔╝██╔═══██╗██║   ██║╚══██╔══╝██║   ██║██╔══██╗██╔════╝    ██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+ ╚████╔╝ ██║   ██║██║   ██║   ██║   ██║   ██║██████╔╝█████╗      ██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║█████╗  ██████╔╝
+  ╚██╔╝  ██║   ██║██║   ██║   ██║   ██║   ██║██╔══██╗██╔══╝      ██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗
+   ██║   ╚██████╔╝╚██████╔╝   ██║   ╚██████╔╝██████╔╝███████╗    ██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║
+   ╚═╝    ╚═════╝  ╚═════╝    ╚═╝    ╚═════╝ ╚═════╝ ╚══════╝    ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                                                                                                                       
+"""
+
+print(Fore.GREEN+(banner))
+
+parser = argparse.ArgumentParser(usage="python3 yt_downloader.py -url {link to download}", description="Use to download YouTube Videos")
+parser.add_argument("-url", type=str, help="The Link to download the item", required=True)
+args = parser.parse_args()
+
+
+
+def run():
+    url = args.url
+    video_url = str(url)
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+
+if __name__=='__main__':
+    run()
